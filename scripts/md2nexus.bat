cb40a17fd0b6f9078cfe4dfc009aa4fa35923220
GroundAura <52499897+GroundAura@users.noreply.github.com>
2023-09-21 04:28:45 -0600

    the update strikes back

--- scripts/md2nexus.bat
+++ scripts/md2nexus.bat
@@ -1,4 +1,5 @@
 @echo off
+title md2nexus
 cd ".."
 
 "C:\Tools\md2nexus\md2nexus" -i "docs/description-md" -o "docs/description-nexus"
--- scripts/test.bat
+++ scripts/test.bat
@@ -1,4 +1,5 @@
 @echo off
+title test
 cd ".."
 
 copy "dist\ProjectName\" "C:\Games\Elder Scrolls\V - Skyrim\SMEFT\mods\ProjectName (pre-release) []""
