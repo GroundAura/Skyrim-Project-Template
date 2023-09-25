c2e6e57d7fa64e5e3f6f0f4a67429d08f95b9871
GroundAura <52499897+GroundAura@users.noreply.github.com>
2023-09-25 10:19:28 -0600

    big organizational and productivity changes

--- .vscode/commandline/md2nexus.bat
+++ .vscode/commandline/md2nexus.bat
@@ -1,6 +1,2 @@
-@echo off
-title md2nexus
-cd ".."
-
 "C:\Tools\md2nexus\md2nexus" -i "docs/description-md" -o "docs/description-nexus"
-
+robocopy "docs\description-md" "docs\description-nexus" *.txt /it
