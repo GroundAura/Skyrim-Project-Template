a46abccf1e6804bb41c47e3f47b1c7d092dcd2c3
GroundAura <52499897+GroundAura@users.noreply.github.com>
2024-03-01 05:30:35 -0700

    update scripts

--- .vscode/scripts/md2nexus.bat
+++ .vscode/scripts/md2nexus.bat
@@ -1,2 +1,2 @@
-"{Pathmd2nexus}\md2nexus" -i "docs\description-md" -o "docs\description-nexus"
+"C:\Games\Skyrim\Tools\md2nexus\md2nexus" -i "docs\description-md" -o "docs\description-nexus"
 robocopy "docs\description-md" "docs\description-nexus" *.txt /it
